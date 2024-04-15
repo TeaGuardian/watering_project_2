18a15130a7fcbdc66e9a45abfff6061f9ced82e1
South Fox <102751028+TeaGuardian@users.noreply.github.com>
2024-04-15 13:45:09 +0500

    Add files via upload

--- python/source/mainwindow.py
+++ python/source/mainwindow.py
@@ -291,5 +291,5 @@ class UI(object):
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Убедитесь, что не используете тестовую (данную) версию в комерческих целях.</p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Разработчик: Юсупов Эрик</p></body></html>"))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Разработчик: --скрыто для соблюдения правил--</p></body></html>"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("Form", "автоматика"))
